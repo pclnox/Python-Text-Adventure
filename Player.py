1de88856f174d3fa68a828af7e49324fc080ef50
endergamerplasma <38787928+endergamerplasma@users.noreply.github.com>
2018-09-23 20:28:18 +0100

    fix

--- Player.py
+++ Player.py
@@ -8,6 +8,8 @@ class Player:
         self.previousX = self.x
         self.previousY = self.y
         self.commandManager = Command.CommandManager(self)
+        self.commandManager = Command.CommandManager()
 
     def Update(self):
         self.commandManager.TestForCommand(self)
+        self.commandManager.TestForCommand()
