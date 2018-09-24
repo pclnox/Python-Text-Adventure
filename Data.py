6f87805fdf4ab35b6e0ac30b523b7d78c897fa0e
pclnox <38761072+pclnox@users.noreply.github.com>
2018-09-24 17:37:17 +0100

    Data Changes

--- Data.py
+++ Data.py
@@ -1,8 +1,5 @@
 from location import Biome
-
-
-startingX = None
-startingY = None
+import random
 
 SHORE = Biome.Biome("shore", 1, 0, 0, 50)
 WOODS = Biome.Biome("woods", 2, 50, 70, 60)
@@ -16,7 +13,18 @@ MAP = [[-2, 1, 1, 1, 1, 1, -2],
        [1,  1, 4, 2, 2, 1,  1],
        [1,  4, 2, 3, 2, 2,  1],
        [1,  4, 3, 5, 5, 5,  1],
-       [1,  0, 3, 5, 6, 5,  1],
+       [1,  3, 3, 5, 6, 5,  1],
        [1,  3, 2, 3, 5, 5,  1],
        [1,  5, 5, 4, 4, 1,  1],
        [1,  1, 1, 1, 1, 1, -2]]
+
+for i in range(20):
+    PLAYER_STARTING_X = random.ranint(0, 6)
+    PLAYER_STARTING_Y = random.randint(0, 7)
+
+    if MAP[PLAYER_STARTING_Y][PLAYER_STARTING_X] == 1:
+        break
+
+    elif i == 19:
+        PLAYER_STARTING_X = 0
+        PLAYER_STARTING_Y = random.randint(1, 6)
