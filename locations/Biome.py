c90b5da2a769a88a4f687a65fc0746ef26839e89
endergamerplasma <38787928+endergamerplasma@users.noreply.github.com>
2018-09-25 19:22:49 +0100

    reference for yay

--- locations/Biome.py
+++ locations/Biome.py
@@ -1,3 +1,5 @@
+import random
+
 class Biome:
     def __init__(self, name, mapValue, lakeProb, groveProb, caveProb):
         self.name = name
@@ -12,3 +14,16 @@ class Biome:
 
     def getEntityProbs(self):
         return self.entityProbs
+
+    '''def FeatureGenerator(self, lakeProb, groveProb, caveProb):
+        lakeCheck = random.randint(0, 100)
+        groveCheck = random.randint(0, 100)
+        caveCheck = random.randint(0, 100)
+        if lakeCheck <= lakeProb:
+            self.lake = True
+
+        if groveCheck <= groveProb:
+            self.grove = True
+
+        if caveCheck <= caveProb:
+            self.cave = True'''
